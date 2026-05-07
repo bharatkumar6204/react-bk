dd99baab1b343671afb17d5337a1cd6bd1a9fa45
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-05-07 21:35:17 +0530

    demo-app

---
+++ demo-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App(){
+  return (
+    <div id="parent">
+      <h1 id="child1">Hello gyus</h1>
+      <h3 id="child2">hello</h3>
+      <h2>This is bharat</h2>
+      
+    </div>
+  )
+}
+
+export default App
