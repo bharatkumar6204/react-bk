815424d68e4887878a08bd4c2dec146f7e554492
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-05-10 21:36:42 +0530

    tailwind-css

---
+++ tailwind-css/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-red-600 '>
+      this is tailwind css
+    </div>
+  )
+}
+
+export default App
